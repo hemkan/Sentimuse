dce9baeaaad0c6717822067f6c8d359b6bc09a31
hemkan <hemkano1403@gmail.com>
2025-02-12 23:45:32 -0500

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Music from "./pages/Music";
+import Poem from "./pages/Poem";
+import Narration from "./pages/Narration";
+import Export from "./components/Export";
+import PageNotFound from "./pages/PageNotFound";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <h1 class="text-3xl font-bold">Vite + React</h1>
-    </>
+    <Routes>
+      <Route path="*" element={<PageNotFound />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/narration" element={<Narration />} />
+      <Route path="/music" element={<Music />} />
+      <Route path="/poem" element={<Poem />} />
+      <Route path="/export" element={<Export />} />
+    </Routes>
   );
-}
+};
 
 export default App;
