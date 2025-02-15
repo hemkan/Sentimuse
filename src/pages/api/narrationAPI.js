9b29f1aa48b919c700135d1a01b6ce78932352c1
GauravKano <gaurav.kanoujia2006@gmail.com>
2025-02-15 01:07:56 -0500

    Created API and streaming for the frontend

---
+++ src/pages/api/narrationAPI.js
@@ -0,0 +1,38 @@
+import { ElevenLabsClient } from "elevenlabs";
+
+const elevenLabs = new ElevenLabsClient({
+  apiKey: process.env.ELEVENLABS_API_KEY,
+});
+
+export default async function getNarrationStream(req, res) {
+  if (req.method == "POST") {
+    const { poetry: text, voice } = req.body;
+
+    if (!text) {
+      return res.status(400).json({ error: "No Text Provided" });
+    }
+
+    try {
+      const audio = await elevenLabs.generate({
+        voice,
+        text,
+        model_id: "eleven_turbo_v2",
+      });
+
+      res.status(200);
+      res.setHeader("Content-Type", "audio/mpeg");
+
+      audio.pipe(res);
+
+      await new Promise((resolve, reject) => {
+        audio.on("end", resolve);
+        audio.on("error", reject);
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: "An Error Occurred" });
+    }
+  } else {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
--- src/pages/narration.js
+++ src/pages/narration.js
@@ -1,5 +1,98 @@
+import { useState } from "react";
+
 const Narration = () => {
-  return <div>Narration</div>;
+  const [narrationBlob, setNarrationBlob] = useState(null);
+  const [input, setInput] = useState("");
+
+  const hoverNarration = async (voice, poetry) => {
+    try {
+      const response = await fetch("../api/narrationAPI", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          voice,
+          poetry,
+        }),
+      });
+
+      if (!response.ok) {
+        const responseError = await response.json();
+        throw new Error(responseError.error);
+      }
+
+      const audio = new Audio();
+      const mediaSource = new MediaSource();
+      audio.src = URL.createObjectURL(mediaSource);
+
+      const rawAudio = [];
+
+      mediaSource.addEventListener("sourceopen", async () => {
+        const sourceBuffer = mediaSource.addSourceBuffer("audio/mpeg");
+
+        const reader = response.body.getReader();
+
+        while (true) {
+          const { value, done } = await reader.read();
+
+          if (value) {
+            if (sourceBuffer.updating) {
+              await new Promise((resolve) => {
+                sourceBuffer.addEventListener("updateend", resolve, {
+                  once: true,
+                });
+              });
+            }
+
+            sourceBuffer.appendBuffer(value);
+
+            if (audio.paused) {
+              audio.play();
+            }
+
+            rawAudio.push(value);
+          }
+
+          if (done) {
+            if (sourceBuffer.updating) {
+              await new Promise((resolve) => {
+                sourceBuffer.addEventListener("updateend", resolve, {
+                  once: true,
+                });
+              });
+            }
+            mediaSource.endOfStream();
+
+            setNarrationBlob(new Blob(rawAudio));
+            break;
+          }
+        }
+      });
+
+      // audio.play();
+    } catch (error) {
+      console.error("WOMP WOMP. AN ERROR OCCURED: " + error.message || error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          hoverNarration("NFG5qt843uXKj4pFvR7C", input);
+        }}
+      >
+        Play
+      </button>
+    </div>
+  );
 };
 
 export default Narration;
