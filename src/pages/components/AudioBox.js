662c702535c0f49a8f31fbf836aba93658a0bd20
ethan mckissic <mckissicethan@gmail.com>
2025-03-26 20:26:30 -0400

    minor size changes

--- src/pages/components/AudioBox.js
+++ src/pages/components/AudioBox.js
@@ -1,14 +1,14 @@
 export default function AudioBox(){
     return (
-        <div className="flex flex-col gap-12 items-center justify-center w-[76%] h-full">
+        <div className="flex flex-col gap-12 items-center justify-center w-[55%] h-full">
             <div className="w-full text-white text-[40px] font-[400] font-[Inria Sans] ">
                 Set the Rhythm
             </div>
-            <div className=" h-[45%] gap-8 w-full flex flex-row justify-center items-center">
-                <div style={{width: '100%', height: '100%', background: '#3A141E', borderRadius: 20}} />
-                <div style={{width: '100%', height: '100%', background: '#3A141E', borderRadius: 20}} />
-                <div style={{width: '100%', height: '100%', background: '#3A141E', borderRadius: 20}} />
-                <div style={{width: '100%', height: '100%', background: '#3A141E', borderRadius: 20}} />
+            <div className=" h-[40%] gap-10 w-full flex flex-row justify-center items-center">
+                <div style={{width: '30%', height: '80%', background: '#3A141E', borderRadius: 20}} />
+                <div style={{width: '30%', height: '80%', background: '#3A141E', borderRadius: 20}} />
+                <div style={{width: '30%', height: '80%', background: '#3A141E', borderRadius: 20}} />
+                <div style={{width: '30%', height: '80%', background: '#3A141E', borderRadius: 20}} />
             </div>
             <div className="text-center justify-center text-white text-2xl font-normal font-['Inria_Sans']">+ Add your own</div>
             <div button className="flex flex-col flex-end items-end w-full"><button className="text-2xl h-full">Next</button></div>
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,7 +1,7 @@
 export default function MainHeader() {
 
     return (
-        <div className="px-8 w-[full] h-[132px] border-b-solid border-b-[1px] border-b-white/50">
+        <div className="px-8 w-[full] h-[100px] border-b-solid border-b-[1px] border-b-white/50">
             <div style={{width: '100%', height: '100%', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 32, fontFamily: 'Inria Sans', fontWeight: '400', wordWrap: 'break-word'}}>Sentimuse</div>        
         </div>
     );
