4d6c5b5348a533038991672cda95d0a36aca8bf2
GauravKano <gaurav.kanoujia2006@gmail.com>
2025-04-17 13:25:16 -0400

    Fix context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,18 @@
 import { Geist } from "next/font/google";
 import "@/styles/globals.css";
+import { PoemProvider } from "@/context/poemContext";
 
 const geist = Geist({
   subsets: ["latin"],
-  weight: ['100', '200', '300', '400', '700'],
+  weight: ["100", "200", "300", "400", "700"],
 });
-
-
 
 export default function App({ Component, pageProps }) {
   return (
+    <PoemProvider>
       <main className={geist.className}>
-            <Component {...pageProps} />
+        <Component {...pageProps} />
       </main>
-    
+    </PoemProvider>
   );
 }
